aea4a7613880854eec81055a9e6f4c81c11cc162
Edgar Pino <edgarbeltran@me.com>
2018-03-23 13:06:25 -0600

    remove https

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :img_to_txt, ImgToTxtWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "image-to-text.edgardev.com", port: 443],
+  url: [scheme: "http", host: "image-to-text.edgardev.com", port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")

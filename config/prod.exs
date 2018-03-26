e8c9a3a6f884411c17f16354c41d125e106d936a
Edgar Pino <edgarbeltran@me.com>
2018-03-25 19:49:27 -0600

    remove force_ssl

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,6 @@ use Mix.Config
 config :img_to_txt, ImgToTxtWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "image-to-text.edgardev.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil, hsts: true],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 

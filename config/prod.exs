6613c24dcec05776b319e5662231c7ccba353b28
Edgar Pino <edgarbeltran@me.com>
2018-03-25 19:44:13 -0600

    remove scheme

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :img_to_txt, ImgToTxtWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "image-to-text.edgardev.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil],
+  url: [host: "image-to-text.edgardev.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil, hsts: true],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 

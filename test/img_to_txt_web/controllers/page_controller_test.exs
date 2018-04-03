07428120a836770e812be38fd0f618c1ef6fd4db
Edgar Pino <edgarbeltran@me.com>
2018-04-03 09:41:20 -0600

    remove useless test

--- test/img_to_txt_web/controllers/page_controller_test.exs
+++ test/img_to_txt_web/controllers/page_controller_test.exs
@@ -5,18 +5,4 @@ defmodule ImgToTxtWeb.PageControllerTest do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Convert Image to Text"
   end
-
-  test "POST /", %{conn: conn} do
-    upload = %Plug.Upload{
-      content_type: "image/png",
-      path: "test/assets/test.png",
-      filename: "test.png"
-    }
-
-    conn = post(conn, "/", %{:image => upload})
-    assert html_response(conn, 200) =~ "An Elixir Test"
-
-    # Sleep to avoid API limit
-    Process.sleep(2000)
-  end
 end

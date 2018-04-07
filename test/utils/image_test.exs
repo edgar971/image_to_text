7d448b0b63ae3cc08fc5e32e7d83b2e346e47605
Edgar Pino <edgarbeltran@me.com>
2018-04-06 22:43:14 -0600

    test for missing file

--- test/utils/image_test.exs
+++ test/utils/image_test.exs
@@ -2,12 +2,18 @@ defmodule ImgToTxt.Utils.ImageTest do
   use ExUnit.Case
 
   alias ImgToTxt.Utils.Image
-  
+
   describe "resize" do
     test "success" do
       expected_path = "test/assets/test-resized.png"
-      %{path: path} = Image.resize("test/assets/test.png", 200, 200, [path: expected_path])
+      %{path: path} = Image.resize("test/assets/test.png", 200, 200, path: expected_path)
       assert path == expected_path
+    end
+
+    test "missing file" do
+      assert_raise File.Error, fn ->
+        Image.resize("test/assets/missing.png", 200, 200)
+      end
     end
   end
 end
